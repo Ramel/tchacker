2f4e6a222e6b15e10bd40695a51d11f5dd6cf5cd
Armel FORTUN <armel@maar.fr>
2009-05-29 19:55:03 +0200

    Change the args order for send_email

--- issue.py
+++ issue.py
@@ -288,7 +288,7 @@ class Tchack_Issue(Issue):
                 continue
             to_addr = user.get_property('email')
             from_addr = MSG(u'please_use_the_website_to_reply@tchack.com')
-            root.send_email(from_addr, to_addr, subject, text=body)
+            root.send_email(to_addr, subject, from_addr, text=body)
 
 
     def get_diff_with(self, record, context):

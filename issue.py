5c5e62bf4cd621871f742c3156aa6aa819491595
Armel FORTUN <armel@maar.fr>
2011-03-10 22:57:58 +0100

    Attempt to add Thumbs to images that haven't one

--- issue.py
+++ issue.py
@@ -421,12 +421,66 @@ class Tchack_Issue(Issue):
                         thumb[1] = True
                     except LookupError:
                         print("LookupError, need to create thumnails for '%s'" % file)
+                        thumb[1] = False
+                elif (not thumb[0][1] and not thumb[1][1] and not thumb[2][1]):
+                    dirname = mkdtemp('makethumbs', 'ikaaro')
+                    tempdir = vfs.open(dirname)
+                    # Paste the file in the tempdir
+                    tmpfolder = "%s" % (dirname)
+                    tmp_uri = ("%s%s%s" % (tmpfolder, sep, name))
+                    tmpfile = open("%s" % tmp_uri, "w+")
+                    tmpfile.write(body)
+                    tmpfile.close()
+
+                    low = 256, 256
+                    med = 800, 800
+                    hig = 1024, 1024
+
+                    # Create the thumbnail PNG resources
+                    thumbext = (["_HIG", hig], ["_MED", med], ["_LOW", low])
+
+                    uri = tmpfolder + sep
+
+                    for te in thumbext:
+                        try:
+                            im = PILImage.open(tmp_uri)
+                        except IOError:
+                            print("IOError = %s" % tmp_uri)
+                        im.thumbnail(te[1], PILImage.ANTIALIAS)
+                        ima = name + te[0]
+                        # Some images are in CMYB, force RVB if needed
+                        if im.mode != "RGB":
+                            im = im.convert("RGB")
+                        im.save(uri + ima + ".jpg", 'jpeg', quality=85)
+                        # Copy the thumb content
+                        thumb_file = tempdir.open(ima + ".jpg")
+                        try:
+                            thumb_data = thumb_file.read()
+                        finally:
+                            thumb_file.close()
+                        format = 'image/jpeg'
+                        cls = get_resource_class(format)
+                        imageThumb = self.make_resource(
+                                    ima,
+                                    cls, 
+                                    body=thumb_data,
+                                    filename=ima,
+                                    extension='jpg',
+                                    format=format
+                                    )
+                        is_thumb = Property(True)
+                        imageThumb.set_property('is_thumb', is_thumb)
+                    has_thumb = Property(True)
+                    file.set_property('has_thumb', has_thumb)
+                    # Clean the temporary folder
+                    vfs.remove(dirname)
+                
                 """ 
                 elif (self.get_resource('%s_LOW' % file) and
                     self.get_resource('%s_MED' % file) and
                     self.get_resource('%s_HIG' % file)):
                 """
-                elif thumb[0][1] and thumb[1][1] and thumb[2][1] and:
+                elif (thumb[0][1] and thumb[1][1] and thumb[2][1]):
                     print("Update Image: %s" % attfile.name)
                     self.get_resource(file).set_property('has_thumb', True)
                     self.get_resource(file).del_property('thumbnail')

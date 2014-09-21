da46aa546218dcc292f3f7ff3b11b2f83b055f63
Armel FORTUN <armel@tchack.com>
2014-09-21 10:23:37 +0200

    In widegts, progressbar inheritance

--- widgets.py
+++ widgets.py
@@ -14,6 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# Import from the Standard Library
+from random import randint
+
 from itools.web import get_context
 
 # Import from tchacker

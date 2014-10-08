b0f1d17707735941d6508748d6cf9cae327eede6
Armel FORTUN <armel@tchack.com>
2014-10-08 16:47:45 +0200

    JS: working with selects

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-var assigned_to,
+ivar assigned_to,
 assigned_list;
-var ori_assigned_to;
-var previous;
+var ori_assigned_to = new Option();
+var previous,
+actual;
 //var not_assigned_to = new Option();
 $(document).ready(function () {
   //assigned_to = $('#assigned-to').find('select#assigned-to option:selected');
@@ -13,14 +14,15 @@ $(document).ready(function () {
   //console.log("assigned_to.text = " + assigned_to[0].text);
   $('#assigned-to').find('select#assigned-to').focus(function () {
     // Store the current value on focus, before it changes
-    previous = $(this).find('option:selected');
+    previous = $(this).find('option:selected').clone();
+    console.log('focus.previous.text = ' + previous.text());
   }).change(function () {
     // Do soomething with the previous value after the change
     //document.getElementById("log").innerHTML = "<b>Previous: </b>"+previous;
-    console.log('previous.text = ' + previous.text());
-    previous = $(this).find('option:selected')
-
-    var cc_list = $('#cc-list').find('SELECT#cc-list OPTION');
+    actual = $(this).find('option:selected').clone()
+    console.log('NOW::previous.text = ' + previous.text());
+    console.log('NOW::previous.text = ' + previous.text());
+    var cc_list = $('#cc-list').find('SELECT#cc-list OPTION').clone();
     // Remove the new one
     for (var i = 0; i < cc_list.length; i++) {
       //console.log("not_assigned_to["+i+"]" + not_assigned_to[i][1]);
@@ -28,15 +30,39 @@ $(document).ready(function () {
       if (cc_list[i].text == '------') {
         continue;
       }
-      console.log('LOOP:cc_list[' + i + '] = ' + cc_list[i].text);
-      if (cc_list[i].text == previous.text()) {
+      console.log('LOOP_1:cc_list[' + i + '] = ' + cc_list[i].text);
+      if (cc_list[i].text == actual.text()) {
         // Remove the use as it is actually selected in assigned_to
         //$('#cc-list').find('SELECT#cc-list option[text="'+ assigned_to[0].text +'"]').remove();
         $('#cc-list').find('SELECT#cc-list option[text="' + cc_list[i].text + '"]').remove();
         console.log('remove(' + cc_list[i].text + ')');
-        //console.log("cc-list[" + i + "] = " + assigned_to[0].text + " == " + cc_list[i].text );  
+        //console.log("cc-list[" + i + "] = " + assigned_to[0].text + " == " + cc_list[i].text );
       }
     }
+    var is_here = false;
+    cc_list = $('#cc-list').find('SELECT#cc-list OPTION').clone();
+    console.log('NOW::previous.text = ' + previous.text());
+    // Add the selected one if not exists
+    for (var i = 0; i < cc_list.length; i++) {
+      //console.log("not_assigned_to["+i+"]" + not_assigned_to[i][1]);
+      // If exist in the actual list
+      if (cc_list[i].text == '------') {
+        continue;
+      }
+      console.log('LOOP_2:cc_list[' + i + '] = ' + cc_list[i].text);
+      if (cc_list[i].text == previous.text()) {
+        // Remove the use as it is actually selected in assigned_to
+        //$('#cc-list').find('SELECT#cc-list option:[text="'+ assigned_to.text +'"]').remove();
+        is_here = true;
+        //ori_assigned_to = cc_list[i];
+        console.log('LOOP_2:cc_list[' + i + '] = assigned_to.text is already there: ' + cc_list[i].text);
+      }
+    }
+    console.log('is_here = ' + is_here);
+    if (is_here == false) {
+      $('#cc-list').find('SELECT#cc-list').append(previous);
+      console.log('append(' + previous.text() + ')');
+    }
+    previous = $(this).find('option:selected').clone();
   });
 });
-

d2e487a0085939a5f30ad0b487cd30138c508a02
Armel FORTUN <armel@tchack.com>
2014-10-08 17:03:26 +0200

    JS: Do not add if Assigned to is empty, need to sort the cc selected/not

--- test.js
+++ test.js
@@ -1,17 +1,7 @@
-ivar assigned_to,
-assigned_list;
-var ori_assigned_to = new Option();
 var previous,
 actual;
-//var not_assigned_to = new Option();
+
 $(document).ready(function () {
-  //assigned_to = $('#assigned-to').find('select#assigned-to option:selected');
-  //assigned_list = $('#assigned-to').find('select#assigned-to');
-  //assigned_to.text = $('#assigned-to').find('select#assigned-to option:selected').attr("text");
-  //ori_assigned_to = $('#assigned-to').find('select#assigned-to option:selected');
-  //ori_assigned_to.text = $('#assigned-to').find('select#assigned-to option:selected').attr("text");
-  //not_assigned_to = $('SELECT#assigned-to, #assigned-to option:not(:selected)');
-  //console.log("assigned_to.text = " + assigned_to[0].text);
   $('#assigned-to').find('select#assigned-to').focus(function () {
     // Store the current value on focus, before it changes
     previous = $(this).find('option:selected').clone();
@@ -25,7 +15,6 @@ $(document).ready(function () {
     var cc_list = $('#cc-list').find('SELECT#cc-list OPTION').clone();
     // Remove the new one
     for (var i = 0; i < cc_list.length; i++) {
-      //console.log("not_assigned_to["+i+"]" + not_assigned_to[i][1]);
       // If exist in the actual list
       if (cc_list[i].text == '------') {
         continue;
@@ -33,10 +22,8 @@ $(document).ready(function () {
       console.log('LOOP_1:cc_list[' + i + '] = ' + cc_list[i].text);
       if (cc_list[i].text == actual.text()) {
         // Remove the use as it is actually selected in assigned_to
-        //$('#cc-list').find('SELECT#cc-list option[text="'+ assigned_to[0].text +'"]').remove();
         $('#cc-list').find('SELECT#cc-list option[text="' + cc_list[i].text + '"]').remove();
         console.log('remove(' + cc_list[i].text + ')');
-        //console.log("cc-list[" + i + "] = " + assigned_to[0].text + " == " + cc_list[i].text );
       }
     }
     var is_here = false;
@@ -44,25 +31,21 @@ $(document).ready(function () {
     console.log('NOW::previous.text = ' + previous.text());
     // Add the selected one if not exists
     for (var i = 0; i < cc_list.length; i++) {
-      //console.log("not_assigned_to["+i+"]" + not_assigned_to[i][1]);
-      // If exist in the actual list
       if (cc_list[i].text == '------') {
         continue;
       }
       console.log('LOOP_2:cc_list[' + i + '] = ' + cc_list[i].text);
       if (cc_list[i].text == previous.text()) {
-        // Remove the use as it is actually selected in assigned_to
-        //$('#cc-list').find('SELECT#cc-list option:[text="'+ assigned_to.text +'"]').remove();
         is_here = true;
-        //ori_assigned_to = cc_list[i];
         console.log('LOOP_2:cc_list[' + i + '] = assigned_to.text is already there: ' + cc_list[i].text);
       }
     }
     console.log('is_here = ' + is_here);
-    if (is_here == false) {
+    if (is_here == false && previous.text() != '') {
       $('#cc-list').find('SELECT#cc-list').append(previous);
       console.log('append(' + previous.text() + ')');
     }
     previous = $(this).find('option:selected').clone();
   });
 });
+);
